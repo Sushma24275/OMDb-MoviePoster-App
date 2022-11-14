2219c1ab013f08b01613f8299126708f8861af10
sushma-fewcents <sushma7susan@gmail.com>
2022-11-14 13:31:10 +0530

    deployment fix

--- src/api/omdbApi.js
+++ src/api/omdbApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 const apiKey = process.env.REACT_APP_OMDB_API_KEY;
 
-export const baseURL = `http://www.omdbapi.com/?apikey=${apiKey}&`;
+export const baseURL = `https://www.omdbapi.com/?apikey=${apiKey}&`;
 
 export const getMovieDetails = async (movieTitle, page) => {
   const response = await axios.get(`${baseURL}s=${movieTitle}&page=${page}`);
